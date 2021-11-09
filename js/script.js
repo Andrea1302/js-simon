a86109861a795a9904ca0e0087e05c967666a8c3
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-11-09 13:38:40 +0100

    ragionamento logico e creazione html

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*Visualizzare in pagina 5 numeri casuali.
+Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
+
+
+// 1 Richiamare la variabile dove scrivere l html al suo interno
+// 2 Generare 5 numeri random ( non duplicati )
+// 3inserire nell html i 5 numeri generati
+// 4 creare timer di 30 secondi dove inserire all inerno il prompt per chiedere di scrivere i numeri visti
+// 5 salvare in variabili le risposte dell utente
+// 5.2 confrontare le risposte dell utente con i numeri generati random
+// 5.3 Stampare in pagina i numeri indovinati se esistono.
