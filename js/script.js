1d38244476db12e1548219cdbe7ee333e3a9a34f
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-11-09 14:07:24 +0100

    Generati i numeri e fatti apparire in pagina

--- js/script.js
+++ js/script.js
@@ -11,3 +11,25 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 // 5 salvare in variabili le risposte dell utente
 // 5.2 confrontare le risposte dell utente con i numeri generati random
 // 5.3 Stampare in pagina i numeri indovinati se esistono.
+
+
+const container = document.getElementById("container");
+
+
+
+// genero 5 numeri random 
+const numeriRandom = [];
+console.log(numeriRandom.length);
+while ( numeriRandom.length != 5 ){
+    let numero = Math.floor(Math.random()*100 + 1);
+
+    let duplicato = numeriRandom.includes(numero);
+
+    if ( duplicato === false ) {
+        numeriRandom.push(numero)
+    }
+}
+console.log(numeriRandom);
+
+container.innerHTML += `${numeriRandom[0]}  ${numeriRandom[1]}  ${numeriRandom[2]}  ${numeriRandom[3]}  ${numeriRandom[4]}`
+
